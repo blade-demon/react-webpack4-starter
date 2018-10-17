3fe940152ca922631ea93673b7719c0c5f760e15
xuziwei <xuziwei89@gmail.com>
2018-10-17 12:29:22 +0800

    add uglifyjs-webpack-plugin

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import "./styles/style.sass";
-// import { add } from "lodash/add";
-import("./another-module.js").then(data => {
-  console.log(data);
-});
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./components/App";
+// import "./styles/style.sass";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+// import("./another-module.js").then(data => {
+//   console.log(data);
+// });
 
-// const sleep = async timeOut =>
-//   new Promise(resolve => {
-//     setTimeout(resolve, timeOut);
-//   });
+// ReactDOM.render(<App />, document.getElementById("app"));
+// import { add, uniq } from "lodash-es";
+const sleep = async timeOut =>
+  new Promise(resolve => {
+    setTimeout(resolve, timeOut);
+  });
 
-// (async () => {
-//   for (var i = 0; i < 5; i++) {
-//     await sleep(1000);
-//     console.log(new Date(), i);
-//   }
+(async () => {
+  for (var i = 0; i < 5; i++) {
+    await sleep(1000);
+    console.log(new Date(), i);
+  }
 
-//   console.log(add(3, 3));
-// })();
+  // console.log(add(3, 3));
+  // console.log(uniq([1, 2, 3, 1]));
+})();
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,15 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
-  devtool: "cheap-module-source-map"
+  devtool: "cheap-module-source-map",
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        test: /\.js(\?.*)?$/i
+      })
+    ]
+  }
 });
